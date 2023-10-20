78fdd4329b5c8fbb4450c9c1b0f23f12719287a1
SreenathSegar <a00255694@mycambrian.ca>
2023-10-20 14:10:50 -0400

    Activity Suggestion Model added

---
+++ ForecastFavorApp/Models/ActivitySuggestion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForecastFavorApp.Models
+{
+    /// <summary>
+    /// Represents user-specific activity suggestions.
+    /// 
+    /// Fields:
+    /// - Id: A unique identifier for each activity suggestion.
+    /// - ActivityType: The type of activity recommended (e.g., hiking, biking).
+    /// - WeatherConditions: Associated weather conditions recommended for the activity.
+    /// - AdditionalInformation: Any extra information pertinent to the activity suggestion.
+    /// </summary>
+
+
+    public class ActivitySuggestion
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string ActivityType { get; set; }
+        public string WeatherConditions { get; set; }
+        public string AdditionalInformation { get; set; }
+    }
+
+
+}
