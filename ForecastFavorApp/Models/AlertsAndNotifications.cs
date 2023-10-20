9e27392527e884a927fcf1e3b50264f4a375775a
SreenathSegar <a00255694@mycambrian.ca>
2023-10-20 13:32:30 -0400

    Alert and Notification Model added

---
+++ ForecastFavorApp/Models/AlertsAndNotifications.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForecastFavorApp.Models
+{
+    /// <summary>
+    /// Represents user-specific Alert and Notifications for the application.
+    /// 
+    /// Fields:
+    /// - Id: A unique identifier for the user's preferences.
+    /// - NotificationPreferences: List of weather conditions the user wants to be notified about.
+    /// - ReceiveStormNotifications: Indicates whether the user wants to receive storm notifications.
+    /// - ReceiveSunnyDayNotifications: Indicates whether the user wants to receive sunny day notifications.
+    /// - ReceiveRainyDayNotifications: Indicates whether the user wants to receive rainy day notifications.
+    /// </summary>
+
+    public class AlertsAndNotifications
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public List<NotificationType> NotificationPreferences { get; set; } = new List<NotificationType>();
+
+        public bool ReceiveStormNotifications { get; set; } = false;
+
+        public bool ReceiveSunnyDayNotifications { get; set; } = false;
+
+        public bool ReceiveRainyDayNotifications { get; set; } = false;
+    }
+}
