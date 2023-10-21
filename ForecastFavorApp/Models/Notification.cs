15636b8888360db5b3a7346bab2e2880b5a79fd2
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-10-20 23:43:06 -0400

    Added Notification model to the project. Defined class structure for user notifications.

---
+++ ForecastFavorApp/Models/Notification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ForecastFavorApp.Models
+{
+    /// <summary>
+    /// Represents a notification sent to the user.
+    /// 
+    /// Properties:
+    /// - Id: A unique identifier for each notification.
+    /// - NotificationType: The type of the notification (e.g., Weather Alert, Reminder).
+    /// - Message: Content/message of the notification.
+    /// - Time: Timestamp when the notification was created/sent.
+    /// </summary>
+
+    public class Notification
+	{
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string NotificationType { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
+
