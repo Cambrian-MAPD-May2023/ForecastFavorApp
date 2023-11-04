950991f8c111eea8f74fcd580ae934820756a124
Dennis Padilla <d_padilla.y@hotmail.com>
2023-11-03 23:53:43 -0400

    Add WeatherService to fetch weather data from API
    This commit introduces the WeatherService class, responsible for
    communicating with the weather API to fetch current weather conditions.
    The service is designed to handle API endpoint construction and
    asynchronous web requests to obtain weather data in JSON format,
    which is then deserialized into the weatherData model.

---
+++ ForecastFavorApp/Services/WeatherService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Diagnostics;
+using ForecastFavorApp.Models;
+using ForecastFavorApp;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+
+public class WeatherService
+{
+    private readonly HttpClient _httpClient;
+
+    public WeatherService()
+    {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri(Constants.API_BASE_URL);
+    }
+
+    public async Task<weatherData> GetWeatherInformation(string location)
+    {
+        if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            return null;
+
+        try
+        {
+            string url = $"{Constants.API_BASE_URL}/current.json?key={Constants.API_KEY}&q={location}&aqi=yes";
+            var response = await _httpClient.GetFromJsonAsync<weatherData>(url);
+            return response;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"An error occurred: {ex.Message}");
+            return null;
+        }
+    }
+}
