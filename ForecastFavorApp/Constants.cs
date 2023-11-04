d3a62067f8597d40905995c561ac14133a6cdb6b
Dennis Padilla <d_padilla.y@hotmail.com>
2023-11-03 23:59:34 -0400

    Add Constants class for centralized API configuration
    This commit introduces the Constants class, which serves as a central repository for storing constant values that are used throughout the application, specifically for configuring the API calls to the weather service.
    
    Key components of the Constants class:
    - `API_KEY`: A constant holding the API key for authenticating requests to the weather service API.
    - `API_BASE_URL`: The base URL for the weather service API, ensuring a single source of truth for the endpoint references.
    
    With this Constants class in place, it facilitates maintainability and scalability of the application by centralizing the configuration. Changes to the API key or the base URL now require updates in only one location, minimizing the risk of discrepancies and bugs related to API access scattered across the codebase.
    
    The use of a Constants class reflects best practices in software development by promoting reusability and ease of management for constant values used in network operations.

---
+++ ForecastFavorApp/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForecastFavorApp
+{
+    internal class Constants
+    {
+
+        public const string API_KEY = "bcea99f817174902bdc03259230311";
+        public const string API_BASE_URL = "http://api.weatherapi.com/v1";
+    }
+}
