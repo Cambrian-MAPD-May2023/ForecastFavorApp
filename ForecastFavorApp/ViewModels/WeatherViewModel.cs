8be05ae853f332223dcec795376f5b2864486b62
Dennis Padilla <d_padilla.y@hotmail.com>
2023-11-03 23:55:03 -0400

    Implement WeatherViewModel for data binding and UI interaction
    This commit introduces the WeatherViewModel, part of the MVVM architecture,
    which facilitates the data binding between the WeatherService and the UI.
    The ViewModel consumes data provided by the WeatherService and exposes
    properties that the view can bind to, allowing for a responsive and
    interactive user experience.
    
    Key features of the WeatherViewModel:
    - Encapsulation of WeatherService instance to fetch weather data.
    - Observable properties for UI binding such as weather icon, temperature,
      weather description, location output, humidity, cloud cover, and day/night status.
    - An ICommand implementation with FetchWeatherInformation method to
      asynchronously retrieve weather data and update the view model properties.
    
    The FetchWeatherInformation method is bound to the view to be triggered by
    the user, ensuring that the latest weather data is presented in the UI upon request.
    This method populates the ViewModel's properties with the data from the weatherData
    model, which the view can then display.
    
    This ViewModel serves as a crucial link between the view and the service layer,
    providing a clean separation of concerns and making the UI more maintainable
    and testable.

---
+++ ForecastFavorApp/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using ForecastFavorApp.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ForecastFavorApp.ViewModels
+{
+    internal partial class WeatherViewModel : ObservableObject
+
+    {
+        private readonly WeatherService _weatherService;
+
+        public WeatherViewModel()
+        {
+            _weatherService = new WeatherService();
+        }
+        [ObservableProperty]
+        private string locationInput;
+
+        [ObservableProperty]
+        private string weatherIcon;
+
+        [ObservableProperty]
+        private string temperature;
+
+        [ObservableProperty]
+        private string weatherDescription;
+
+        [ObservableProperty]
+        private string locationOutput;
+
+        [ObservableProperty]
+        private string humidity;
+
+        [ObservableProperty]
+        private string cloudCoverLevel;
+
+        [ObservableProperty]
+        private string isDay;
+
+        [RelayCommand]
+        public async Task FetchWeatherInformation()
+        {
+            var weatherData = await _weatherService.GetWeatherInformation(LocationInput);
+            if (weatherData != null)
+            {
+                //Assign values to ViewModel properties accordingly
+
+                WeatherIcon = weatherData.Current.Condition.Icon;
+                Temperature = $"{weatherData.Current.TemperatureCelsius}°C / {weatherData.Current.TemperatureFahrenheit}°F";
+                WeatherDescription = weatherData.Current.Condition.Text;
+                LocationOutput = $"{weatherData.Location.Name}, {weatherData.Location.Region}, {weatherData.Location.Country}";
+                Humidity = $"{weatherData.Current.Humidity}%";
+                CloudCoverLevel = $"{weatherData.Current.Cloud}%";
+                IsDay = weatherData.Current.IsDay == 1 ? "Day" : "Night";
+
+            }
+
+        }
+
+    }
+}
