8e045bfc5b97fa64d96fc8fade300598fb2914a1
Dennis Padilla <d_padilla.y@hotmail.com>
2023-11-03 23:56:49 -0400

    Add WeatherPage view with associated ViewModel binding
    This commit introduces the WeatherPage, a user interface designed using XAML,
    which is bound to the WeatherViewModel. This page displays real-time weather
    information fetched from the WeatherService.
    
    Key highlights of the WeatherPage:
    - XAML layout with a `VerticalStackLayout` containing controls for user input,
      weather information display, and additional weather details.
    - Data binding expressions connecting the XAML controls to the WeatherViewModel's
      properties, ensuring that UI updates reflect the ViewModel state.
    - A command binding for a button to trigger the asynchronous fetch of weather
      data from the WeatherViewModel.
    - Initialisation of the WeatherViewModel and setting it as the BindingContext of
      the page to enable the data binding mechanism.
    
    The code-behind (`WeatherPage.xaml.cs`) sets up the ViewModel as the BindingContext
    and includes the `OnAppearing` method, which invokes the ViewModel's
    `FetchWeatherInformation` method to retrieve weather data on page appearance.
    
    This WeatherPage provides a rich and interactive user experience, showcasing the
    current weather data and updating dynamically as the ViewModel receives new data.
    It encapsulates the user interface logic, separating it from data fetching and
    business logic contained in the ViewModel, adhering to MVVM design principles.

---
+++ ForecastFavorApp/Views/WeatherPage.xaml.cs
@@ -0,0 +1,35 @@
+using Microsoft.Maui.Controls;
+using System;
+using ForecastFavorApp.ViewModels;
+
+namespace ForecastFavorApp.Views
+{
+    public partial class WeatherPage : ContentPage
+    {
+        WeatherViewModel _viewModel;
+
+        public WeatherPage()
+        {
+            InitializeComponent();
+            _viewModel = new WeatherViewModel();
+            // Assign the ViewModel to the page's BindingContext to enable data binding.
+            BindingContext = _viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            // Make sure to handle exceptions appropriately
+            try
+            {
+                // Assuming FetchWeatherData is an async method that makes the API call and updates the ViewModel.
+                await _viewModel.FetchWeatherInformation();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during the API call.
+                await DisplayAlert("Error", $"An error occurred: {ex.Message}", "OK");
+            }
+        }
+    }
+}
