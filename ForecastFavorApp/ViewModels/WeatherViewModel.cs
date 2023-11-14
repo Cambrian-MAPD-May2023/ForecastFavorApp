82c9793288de92fbfb8d2bc1a82200d44b71642b
Dennis Padilla <d_padilla.y@hotmail.com>
2023-11-13 22:03:56 -0500

    Modified ViewModel to inclue data for Second and Third Day Forecast to display in the 3-day tab.

--- ForecastFavorApp/ViewModels/WeatherViewModel.cs
+++ ForecastFavorApp/ViewModels/WeatherViewModel.cs
@@ -59,13 +59,52 @@ namespace ForecastFavorApp.ViewModels
         private string tomorrowHumidity; // Forecasted humidity for tomorrow.
 
         [ObservableProperty]
+        private string secondDayTemperature; // Forecasted temperature for 2nd day
+
+        [ObservableProperty]
+        private string secondDayWeatherIcon; // URL for 2nd day weather condition icon.
+
+        [ObservableProperty]
+        private string secondDayWeatherDescription; // Textual description of 2nd's day weather.
+
+        [ObservableProperty]
+        private string secondDayHumidity; // Forecasted humidity for 3rd day
+
+        [ObservableProperty]
+        private string thirdDayTemperature; // Forecasted temperature for 3rd day
+
+        [ObservableProperty]
+        private string thirdDayWeatherIcon; // URL for 3rd day weather condition icon.
+
+        [ObservableProperty]
+        private string thirdDayWeatherDescription; // Textual description of 3rd's day weather.
+
+        [ObservableProperty]
+        private string thirdDayHumidity; // Forecasted humidity for 3rd day
+
+        [ObservableProperty]
         private Forecast forecast; // Complete forecast object.
+
+        [ObservableProperty]
+        private ForecastDayDetail todayForecast; // Detailed forecast for today
 
         [ObservableProperty]
         private ForecastDayDetail tomorrowForecast; // Detailed forecast for tomorrow.
 
         [ObservableProperty]
+        private ForecastDayDetail secondDayForecast; // Detailed forecast for the second day.
+
+        [ObservableProperty]
+        private ForecastDayDetail thirdDayForecast; // Detailed forecast for the third day.
+
+        [ObservableProperty]
+        private DateTime currentDate;
+
+        [ObservableProperty]
         private ObservableCollection<ForecastDayDetail> threeDayForecastDetails; // Collection of detailed forecasts for the next three days.
+
+        [ObservableProperty]
+        private ObservableCollection<ForecastHour> hourlyForecast; // Collection of hourly forecast data for the current day.
 
         // Relay Command attribute that defines an asynchronous method for fetching weather information.
         [RelayCommand]
@@ -73,10 +112,11 @@ namespace ForecastFavorApp.ViewModels
         {
             // Calls the weather service to get weather information for the next three days.
             var weatherData = await _weatherService.GetWeatherInformation(LocationInput, 3);
-            if (weatherData?.Forecast?.ForecastDay != null && weatherData.Forecast.ForecastDay.Count >= 2)
+            if (weatherData?.Forecast?.ForecastDay != null && weatherData.Forecast.ForecastDay.Count >= 3)
             {
                 // If the data is successfully retrieved, assigns values to the ViewModel properties.
                 // Current weather data assignments
+                
                 WeatherIcon = "http:" + weatherData.Current.Condition.Icon;
                 Temperature = $"{weatherData.Current.TemperatureCelsius}°C / {weatherData.Current.TemperatureFahrenheit}°F";
                 WeatherDescription = weatherData.Current.Condition.Text;
@@ -84,17 +124,39 @@ namespace ForecastFavorApp.ViewModels
                 Humidity = $"{weatherData.Current.Humidity}%";
                 CloudCoverLevel = $"{weatherData.Current.Cloud}%";
                 IsDay = weatherData.Current.IsDay == 1 ? "Day" : "Night";
+              
 
                 // Tomorrow's weather forecast assignments
-                TomorrowForecast = weatherData.Forecast.ForecastDay[1].Day;
-                TomorrowTemperature = $"{TomorrowForecast.MaxTempC}°C / {TomorrowForecast.MaxTempF}°F";
+                TomorrowForecast = weatherData.Forecast.ForecastDay[0].Day;
+                TomorrowTemperature = $"{TomorrowForecast.AvgTempC}°C / {TomorrowForecast.AvgTempF}°F";
                 TomorrowWeatherIcon = "http:" + TomorrowForecast.Condition.Icon;
                 TomorrowWeatherDescription = TomorrowForecast.Condition.Text;
                 TomorrowHumidity = $"{TomorrowForecast.AvgHumidity}%";
 
-                // Assigns the 3-day weather forecast details.
+                // 2nd Day's weather forecast assignments
+                SecondDayForecast = weatherData.Forecast.ForecastDay[1].Day;
+                SecondDayTemperature = $"{SecondDayForecast.AvgTempC}°C / {SecondDayForecast.AvgTempF}°F";
+                SecondDayWeatherIcon = "http:" + SecondDayForecast.Condition.Icon;
+                SecondDayWeatherDescription = SecondDayForecast.Condition.Text;
+                SecondDayHumidity = $"{SecondDayForecast.AvgHumidity}%";
+
+                // 3rd Day's weather forecast assignments
+                ThirdDayForecast = weatherData.Forecast.ForecastDay[2].Day;
+                ThirdDayTemperature = $"{ThirdDayForecast.AvgTempC}°C / {ThirdDayForecast.AvgTempF}°F";
+                ThirdDayWeatherIcon = "http:" + ThirdDayForecast.Condition.Icon;
+                ThirdDayWeatherDescription = ThirdDayForecast.Condition.Text;
+                ThirdDayHumidity = $"{ThirdDayForecast.AvgHumidity}%";
+
                 ThreeDayForecastDetails = new ObservableCollection<ForecastDayDetail>(
-                    weatherData.Forecast.ForecastDay.Take(3).Select(fd => fd.Day));
+                  weatherData.Forecast.ForecastDay.Take(3).Select(fd => fd.Day));
+
+                // Extract and store the hourly forecast for the current day.
+                HourlyForecast = new ObservableCollection<ForecastHour>(weatherData.Forecast.ForecastDay[0].Hour);
+
+
+
+
+
             }
         }
     }
