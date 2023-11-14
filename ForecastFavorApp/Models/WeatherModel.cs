2ce4cb360aa865e7b3870fe639489fdae2c81a7b
Dennis Padilla <d_padilla.y@hotmail.com>
2023-11-13 22:00:51 -0500

    Added a new porperties to 'ForecastHour' to represent hourly weather forecast data. This class includes properties for temperature in Celsius and Fahrenheit, day/night indicator, weather condition, wind details, pressure, precipitation, humidity, cloud cover, 'feels like' temperature, visibility, UV index, gust speed, and chance of rain and snow.

--- ForecastFavorApp/Models/WeatherModel.cs
+++ ForecastFavorApp/Models/WeatherModel.cs
@@ -108,17 +108,80 @@ namespace ForecastFavorApp.Models
     // Represents the weather forecast for a specific hour.
     public class ForecastHour
     {
-        [JsonPropertyName("time_epoch")]
-        public long TimeEpoch { get; set; }
+        [JsonPropertyName("last_updated_epoch")]
+        public long LastUpdatedEpoch { get; set; }
 
-        [JsonPropertyName("time")]
-        public string Time { get; set; }
+        [JsonPropertyName("last_updated")]
+        public string LastUpdated { get; set; }
 
-        // ... properties similar to the `Current` class for the hourly forecast
         [JsonPropertyName("temp_c")]
         public double TemperatureCelsius { get; set; }
 
-        // ... other relevant properties
+        [JsonPropertyName("temp_f")]
+        public double TemperatureFahrenheit { get; set; }
+
+        [JsonPropertyName("is_day")]
+        public int IsDay { get; set; }
+
+        [JsonPropertyName("condition")]
+        public Condition Condition { get; set; }
+
+        [JsonPropertyName("wind_mph")]
+        public double WindMph { get; set; }
+
+        [JsonPropertyName("wind_kph")]
+        public double WindKph { get; set; }
+
+        [JsonPropertyName("wind_degree")]
+        public int WindDegree { get; set; }
+
+        [JsonPropertyName("wind_dir")]
+        public string WindDirection { get; set; }
+
+        [JsonPropertyName("pressure_mb")]
+        public double PressureMb { get; set; }
+
+        [JsonPropertyName("pressure_in")]
+        public double PressureIn { get; set; }
+
+        [JsonPropertyName("precip_mm")]
+        public double PrecipitationMm { get; set; }
+
+        [JsonPropertyName("precip_in")]
+        public double PrecipitationIn { get; set; }
+
+        [JsonPropertyName("humidity")]
+        public int Humidity { get; set; }
+
+        [JsonPropertyName("cloud")]
+        public int Cloud { get; set; }
+
+        [JsonPropertyName("feelslike_c")]
+        public double FeelsLikeCelsius { get; set; }
+
+        [JsonPropertyName("feelslike_f")]
+        public double FeelsLikeFahrenheit { get; set; }
+
+        [JsonPropertyName("vis_km")]
+        public double VisibilityKm { get; set; }
+
+        [JsonPropertyName("vis_miles")]
+        public double VisibilityMiles { get; set; }
+
+        [JsonPropertyName("uv")]
+        public double UV { get; set; }
+
+        [JsonPropertyName("gust_mph")]
+        public double GustMph { get; set; }
+
+        [JsonPropertyName("gust_kph")]
+        public double GustKph { get; set; }
+
+        [JsonPropertyName("chance_of_rain")]
+        public double ChanceOfRain { get; set; }
+
+        [JsonPropertyName("chance_of_snow")]
+        public double ChanceOfSnow { get; set; }
     }
 
 
