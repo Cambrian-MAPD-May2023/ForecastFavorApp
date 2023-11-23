3093f4426754b8d213485c5927db60bb06d2d77a
Dennis Padilla <d_padilla.y@hotmail.com>
2023-11-23 01:39:47 -0500

    Initialize user preferences on app start from cloud data
    Modified the application startup logic to asynchronously load user preferences from cloud storage. On startup, the app now checks for existing users and initializes the WeatherViewModel with the preferences of the first user found. This update enhances the app's capability to personalize the user experience right from launch. A placeholder method for fetching user preferences from the cloud has been included, setting the stage for future implementation of cloud-based user settings retrieval.

--- ForecastFavorApp/App.xaml.cs
+++ ForecastFavorApp/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using ForecastFavorApp.Views;
+using ForecastFavorApp.Models;
+using ForecastFavorApp.ViewModels;
 namespace ForecastFavorApp
 {
     public partial class App : Application
@@ -8,6 +10,38 @@ namespace ForecastFavorApp
             InitializeComponent();
             MainPage = new NavigationPage(new WeatherPage());
 
+        }
+
+        protected override async void OnStart()
+        {
+            base.OnStart();
+
+            // Attempt to load users from the cloud database.
+            var users = await LoadUsersFromCloud();
+            if (users.Any())
+            {
+                // If users exist, initialize the view model with the first user's preferences.
+                string defaultUsername = users.First().Username;
+                var mainPage = MainPage as NavigationPage;
+                // Make sure the current page is WeatherPage and it's loaded
+                if (mainPage?.CurrentPage is WeatherPage weatherPage)
+                {
+                    // Get the view model from the WeatherPage
+                    if (weatherPage.BindingContext is WeatherViewModel viewModel)
+                    {
+                        await viewModel.InitializeAsync(defaultUsername);
+                    }
+                }
+            }
+            // If no users exist, the WeatherPage will prompt for new user details.
+        }
+
+        // Method to load users from the cloud database.
+        private async Task<IEnumerable<UserPreferences>> LoadUsersFromCloud()
+        {
+            // Implement logic to fetch user preferences from the cloud.
+            // This is a placeholder for actual cloud storage fetching logic.
+            return await Task.FromResult(Enumerable.Empty<UserPreferences>());
         }
     }
 }
